31b0aa14156e7e60bc51161289ff3e7a89f21632
Cesar Kawakami <kawakami@fb.com>
2017-08-16 11:56:26 +0100

    Initial commit

---
+++ copyfiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+user=kawakami
+
+cp -r "/Volumes/Macintosh HD/Users/$user/Desktop/edidfix/DisplayVendorID-"* "/Volumes/Macintosh HD/System/Library/Displays/Contents/Resources/Overrides/"
